431bd021c35303d06746847d3cfc1a64d631c5c3
khanhnq <quockhanhnguyen2882@gmail.com>
2024-06-20 17:32:21 +0700

    Domain-Driven Design for Order Service

---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain.mapper;
+
+import com.food.ordering.system.domain.entity.Order;
+import com.food.ordering.system.domain.entity.Product;
+import com.food.ordering.system.domain.entity.Restaurant;
+import com.food.ordering.system.domain.valueobject.ProductId;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDataMapper {
+
+    public Restaurant createOrderCommandToRestaurant(CreateOrderCommand createOrderCommand) {
+        return Restaurant.builder()
+                .restaurantId(new RestaurantId(createOrderCommand.getRestaurantId()))
+                .products(createOrderCommand.getItems().stream().map(orderItem ->
+                        new Product(new ProductId(orderItem.getProductId()))).toList())
+                .build();
+    }
+
+    public Order createOrderCommandToOrder(CreateOrderCommand createOrderCommand) {
+        return Order.builder().build();
+    }
+
+}
